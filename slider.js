b44a1e6acc09bfcb9d966983328618a3b079b8ca
Vitaly Razumov <vitalik03031998@gmail.com>
2020-07-14 21:26:56 +0300

    Update slider.js

--- slider.js
+++ slider.js
@@ -16,6 +16,7 @@ function mobileSlider() {
         state = true;
     } else { 
         if (mySwiper) {
+            state = false;
             console.log("Destroy")
             mySwiper.destroy()
         } 
@@ -30,23 +31,7 @@ window.addEventListener("resize", () => {
 
 
 
-function mobileSlider() {      
-    let tablet = window.matchMedia('(min-width: 768px'); 
-    console.log("mobile slider");
-    if (!tablet.matches && !state) {
-        console.log('Slider ON') 
-        mySwiper = new Swiper(slider, {
-            slidesPerView: 1,
-            spaceBetween: 1,
-            loop: true, 
-            slideClass: "swiper-slide",
-        });
-        state = true;
-    } else { 
-        if (mySwiper) {
-            console.log("Destroy")
-            mySwiper.destroy()
-        } 
+
 
 
 
