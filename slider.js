453600c717e1562f7a877480267af9778cab30c6
Vitaly Razumov <vitalik03031998@gmail.com>
2020-07-14 21:30:07 +0300

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,33 +1,30 @@
 const slider = document.querySelector(".slider-container");
 let mySwiper;
 let state = false;
-
-function mobileSlider() {      
-    let tablet = window.matchMedia('(min-width: 768px'); 
-    console.log("mobile slider");
-    if (!tablet.matches && !state) {
-        console.log('Slider ON') 
-        mySwiper = new Swiper(slider, {
-            slidesPerView: 1,
-            spaceBetween: 1,
-            loop: true, 
-            slideClass: "swiper-slide",
-        });
-        state = true;
-    } else { 
+function mobileSlider() {
+    let tablet = window.matchMedia('(min-width: 768px');
+    console.log(state);
+    if (!tablet.matches) {
+        if (!state) {
+            state = true;
+            console.log('Slider ON')
+            mySwiper = new Swiper(slider, {
+                slidesPerView: 1,
+                spaceBetween: 1,
+                loop: true,
+                slideClass: "swiper-slide",
+            });
+        }
+    } else {
         if (mySwiper) {
-            state = false;
             console.log("Destroy")
             mySwiper.destroy()
-        } 
-
-
-mobileSlider();
-
-window.addEventListener("resize", () => {
-    mobileSlider();
-});
-
+            state = false;
+        }
+    }
+}
+window.addEventListener('load', mobileSlider);
+window.addEventListener("resize", mobileSlider);
 
 
 
